fdcd351783562cda8638daf5a8708a2c0c406bd6
박 성현 <log_sh@naver.com>
2023-04-13 18:19:12 +0900

    App.js에 전체적인 테마를 설정할 수 있는 ThemeProvider추가

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -4,15 +4,37 @@ import MainPage from "./component/MainPage";
 import Login from "./component/Login";
 import SignUp from "./component/SignUp";
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#ffffff',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
+
 function App() { //kwon bo sung
   return (
     <BrowserRouter>
-      <Appbar />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Appbar />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
--- client/my-app/src/component/AppBar.js
+++ client/my-app/src/component/AppBar.js
@@ -8,8 +8,6 @@ import Button from '@mui/material/Button';
 
 const pages = ['HOME', 'ChatGPT', 'Board'];
 
-//MUI에 있는 App Bar 탭 참고
-
 function Appbar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
@@ -34,7 +32,7 @@ function Appbar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" >
       <Container maxWidth="lg" sx={{ mt: '20px' }}>
         <Toolbar disableGutters>
           <Typography
@@ -48,22 +46,26 @@ function Appbar() {
               fontFamily: 'monospace',
               fontWeight: 700,
               //letterSpacing: '.3rem', 자간설정
-              color: 'inherit',
+              color: 'black',
               textDecoration: 'none',
             }}
           >
             Capstone
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: 'flex-end' } }}>
             {pages.map((page) => (
               <Button
+              
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, display: 'block', color: 'black', fontWeight: 700, mr: '30px'}}
               >
                 {page}
               </Button>
             ))}
+            <Button sx={{ my: 2, display: 'block', border: 1, color: 'black', fontWeight: 700,}}>
+              GET STARTED
+            </Button>
           </Box>
         </Toolbar>
       </Container>
